ba807b9819d48ac5392f3f0b2a04398400443959
Rinbili <rinbili@vip.qq.com>
2022-09-30 19:24:29 +0800

    Test

--- Auto-Post.py
+++ Auto-Post.py
@@ -3,23 +3,52 @@ import os
 import requests
 from bs4 import BeautifulSoup
 from urllib.parse import *
-from pypushdeer import PushDeer
+from Crypto.Cipher import AES
+import random
+import base64
 
 requests.packages.urllib3.util.ssl_.DEFAULT_CIPHERS += "HIGH:!DH:!aNULL"
 
-# URL
+# URLs&&aes_chars
 url = "https://auth.dgut.edu.cn/authserver/oauth2.0/authorize?response_type=code&client_id=1021534300621787136" \
       "&redirect_uri=https://yqfk-daka.dgut.edu.cn/new_login/dgut&state=yqfk "
 url2 = "https://yqfk-daka-api.dgut.edu.cn/auth"
 url3 = "https://yqfk-daka-api.dgut.edu.cn/record"
 url4 = "https://sctapi.ftqq.com/"
+url5 = "https://api2.pushdeer.com/message/push"
+aes_chars = 'ABCDEFGHJKMNPQRSTWXYZabcdefhijkmnprstwxyz2345678'
 
 # secret
-username = os.environ["USERNAME"]
-pw = os.environ["PASSWORD"]
-pushkey = os.environ["PUSHKEY"]
-sendkey = os.environ["SENDKEY"]
+username = os.environ.get("USERNAME")
+pw = os.environ.get("PASSWORD")
+pushkey = os.environ.get("PUSHKEY")
+sendkey = os.environ.get("SENDKEY")
 session = requests.Session()
+
+
+def pad(password):  # 把密码补充成符合 AES-128 CBC 规范形式
+    password = random_str(64) + password
+    password_length = len(password)
+    add_count = AES.block_size - password_length % AES.block_size
+    if add_count == 0:
+        add_count = AES.block_size
+    _pad = chr(add_count)
+    return password + _pad * add_count
+
+
+def random_str(length):
+    ret = ""
+    for i in range(length):
+        ret += random.choice(aes_chars)
+    return ret
+
+
+def password_encrypt(user_password, aes_key):
+    iv = random_str(16)
+    user_password = pad(user_password).encode("utf8")
+    aes_key = str(aes_key.strip())
+    cipher = AES.new(aes_key.encode("utf8"), AES.MODE_CBC, iv.encode("utf8"))
+    return base64.b64encode(cipher.encrypt(user_password))
 
 
 def post_pushdeer(title, message):
@@ -27,8 +56,12 @@ def post_pushdeer(title, message):
     desp = ""
     for item in message:
         desp += "- " + item + "  \n"
-    pushdeer = PushDeer(pushkey=pushkey)
-    pushdeer.send_markdown(title, desp=desp)
+    session.post(url=url5, data={
+        "pushkey": pushkey,
+        "text": title,
+        "desp": desp,
+        "type": "markdown"
+    })
 
 
 def post_server(title, message):
@@ -36,10 +69,10 @@ def post_server(title, message):
     desp = ""
     for item in message:
         desp += "- " + item + "  \n"
-    session.post(url=url4 + sendkey + ".send",
-                 data={
-                     'text': title,
-                     'desp': desp})
+    session.post(url=url4 + sendkey + ".send", data={
+        "text": title,
+        "desp": desp
+    })
 
 
 def post_message(title, message):
@@ -63,8 +96,9 @@ def get_token(message):
         return None
     post_msg("获取encryptSalt&execution成功", message, 0)
 
-    with os.popen("node ./encrypt.js \"{0}\" \"{1}\"".format(pw, encrypt_salt)) as nodejs:
-        password = nodejs.read().replace('\n', '')
+    # with os.popen("node ./encrypt.js \"{0}\" \"{1}\"".format(pw, encrypt_salt)) as nodejs:
+    #     password = nodejs.read().replace('\n', '')
+    password = password_encrypt(pw, encrypt_salt)
 
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
@@ -81,9 +115,7 @@ def get_token(message):
             "lt": "",
             "pwdEncryptSalt": encrypt_salt,
             "execution": execution}
-    response = session.post(url=origin.url,
-                            data=data,
-                            headers=headers)
+    response = session.post(url=origin.url, data=data, headers=headers)
     # 从url中提取参数，作为请求负载，其中参数code对应的应该是token
     token_data = {}
     result = urlparse(response.url)
@@ -102,10 +134,9 @@ def get_access_token(token_data, message):
     headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
                              "Chrome/105.0.0.0 Safari/537.36 Edg/105.0.1343.50",
                "Origin": "https://yqfk-daka.dgut.edu.cn",
-               "Referer": "https://yqfk-daka.dgut.edu.cn/"}
-    access_token_post = session.post(url=url2,
-                                     headers=headers,
-                                     json=token_data)
+               "Referer": "https://yqfk-daka.dgut.edu.cn/"
+               }
+    access_token_post = session.post(url=url2, headers=headers, json=token_data)
     access_token = access_token_post.json().get("access_token")
     if access_token is None:
         post_msg("获取access_token失败", message, 1)
@@ -128,9 +159,7 @@ def post_form(access_token, message):
     # postForm
     user_data["health_situation"] = 1
     user_data["body_temperature"] = 37
-    user_data_post = session.post(url=url3,
-                                  headers=headers,
-                                  json={"data": user_data})
+    user_data_post = session.post(url=url3, headers=headers, json={"data": user_data})
     if user_data_post.status_code != 200:
         if user_data_post.status_code == 400:
             post_msg("提交data表单失败失败:" + user_data_post.json().get("message"), message, 1)
